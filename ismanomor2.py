168b1cafb5ed1c165ac08cb79f28015add20fa25
ismarauu <202210715298@mhs.ubharajaya.ac.id>
2022-10-28 14:17:25 +0700

    Tugas 2 sudah selesai
    kirim

---
+++ ismanomor2.py
@@ -0,0 +1,5 @@
+# pencabangan IF dengan 1 kasus
+nama =input("Masukkan Nama : ")
+if nama == "Isma Raudhatul Janah" :
+    print("Hai", nama, "Selamat Bergabung Di Perusahaan SEJAHTERA ABADI")
+    print()
---
+++ ismanomor4.py
@@ -0,0 +1,18 @@
+# pengulangan pakai for
+for i in range(6):
+    print("pengulangan ke - ", i)
+
+print("\n")
+
+listMakanan = ["Apel", "Manggis", "Markisa", "Anggur", "Jambu Air"]
+for Makanan in listMakanan :
+    print(Makanan)
+
+print("\n")
+
+# pengulangan pakai while
+angka = 1
+
+while angka > 10 :
+    angka += 1
+    print("contoh penggunaan sintax while")
